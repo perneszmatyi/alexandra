e30a1d63ec9232827c3123e31a12c2f2a8c07e1a
perneszmatyi <perneszmatyi@gmail.com>
2025-03-29 17:25:58 +0100

    Create header

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,7 +1,43 @@
+import underline from "../../assets/underline.svg";
+
 export const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <nav className="bg-blue-main w-full p-11 flex justify-center gap-8 items-center">
+      <a className=" text-lg font-medium font-raleway text-lg text-white-text group hover:text-brown-1 cursor-pointer transition-all duration-200">
+        RÓLAM
+        <img
+          className="absolute opacity-0 group-hover:opacity-100 transition-all duration-200"
+          src={underline}
+        />
+      </a>
+      <a className="text-lg font-medium font-raleway text-lg text-white-text group hover:text-brown-1 cursor-pointer transition-all duration-200">
+        BEJELENTKEZÉS
+        <img
+          className="absolute opacity-0 group-hover:opacity-100 transition-all duration-200"
+          src={underline}
+        />
+      </a>
+      <a className="text-lg font-medium font-raleway text-lg text-white-text group hover:text-brown-1 cursor-pointer transition-all duration-200">
+        AKTUALITÁSOK
+        <img
+          className="absolute opacity-0 group-hover:opacity-100 transition-all duration-200"
+          src={underline}
+        />
+      </a>
+      <a className="text-lg font-medium font-raleway text-lg text-white-text group hover:text-brown-1 cursor-pointer transition-all duration-200">
+        KÖNYVESPOLC
+        <img
+          className="absolute opacity-0 group-hover:opacity-100 transition-all duration-200"
+          src={underline}
+        />
+      </a>
+      <a className="text-lg font-medium font-raleway text-lg text-white-text group hover:text-brown-1 cursor-pointer transition-all duration-200">
+        KAPCSOLAT
+        <img
+          className="absolute opacity-0 group-hover:opacity-100 transition-all duration-200"
+          src={underline}
+        />
+      </a>
+    </nav>
   );
 };
