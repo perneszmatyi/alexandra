3eceb170428b81b75e6e153242641f23b160d6da
perneszmatyi <perneszmatyi@gmail.com>
2025-03-29 15:46:57 +0100

    App init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Header, Hero, Info, News, Books } from "./components";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <Info />
+      <News />
+      <Books />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export * from "./Hero/hero";
+export * from "./Info/info";
+export * from "./News/news";
+export * from "./Header/header";
+export * from "./Books/books";
